16934320cb117130c02bc1b1e60b8061575f56c2
Lena Horak <elen.gorak@gmail.com>
2020-04-06 15:48:41 +0300

    work with expected_conditions method text_to_be_present_in_element 1.1

---
+++ lesson2_4_step6.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/cats.html")
+browser.find_element_by_id("button")
+time.sleep(10)
+browser.quit()
